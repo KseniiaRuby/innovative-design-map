5525c31e194299b20d122742a665af0491d40d1b
Kseniia <kseniia.ruby@gmail.com>
2021-06-21 22:07:11 -0600

    added background , centered first page, deleted index.css, added more categories to select

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import background from "./Calgary_DT.jpg"
 
 export function SelectCategory() {
    const [categoryState, setCategoryState] = useState("Category")
 
    return (
-      <div className="header"> What makes Calgary 
+      <div className="header"> What makes Calgary
+      <br></br>
          <select
-            className="custom-select"
+            style={{ fontSize:`0.7em` }}
+            className="custom-select"            
             value={categoryState}
             onChange={(e) => {
                const selectedCategory = e.target.value
@@ -16,6 +19,12 @@ export function SelectCategory() {
          <option value="green">green</option>
          <option value="resilient">resilient</option>
          <option value="adaptive">adaptive</option>
+         <option value="clean">clean</option>
+         <option value="vibrant">vibrant</option>
+         <option value="safe">safe</option>
+         <option value="healthy">healthy</option>
+         <option value="smart">smart</option>
+         <option value="accessible">accessible</option>
          </select> ?
       {categoryState, []}
 </div>
