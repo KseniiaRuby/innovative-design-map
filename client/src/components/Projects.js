02a981fba2e74e7255e843b272923f5b98401cfa
Femi Bode-George <femibodgeorge@gmail.com>
2021-07-09 13:58:07 -0600

    update project list

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,24 +1,33 @@
 export const Projects = {
-   ProjectName: "",
-   Image,
-   Location: {
-      Address: "",
-      coordinates: [],
-   },
-   Neighbourhood: "",
-   DesignersCollaborations: "",
-   Owner: "",
-   CompletionDate: "",
-   TypologyOfProject: "",
-   AccessToPublic: false,
-   ProjectProfile: "",
-   KeyFactsAboutTheProject: "",
-   ContactInformation: {
-      CompanyName: "",
-      FirstName: "",
-      LastName: "",
+   Contact_Information: {
+      Company_Name: "",
+      First_Name: "",
+      Last_Name: "",
       Email: "",
-      PhoneNumber: "",
-      Website: "",
+      Phone_Number: "",
+      Project_Website: "",
    },
-}
+  Project_Information = {  
+     Project_Name: "",
+  Image,
+  Location: {
+    Address: "",
+    City: "",
+    Province: "",
+    Postal_Code: "",
+    Coordinates: [],
+  },
+  Neighbourhood: "",
+  Project_Owners_and_Clients: [],
+  Project_Designers_Collaborations: [],
+  Completion_Date: "",
+  Project_Typology: [],
+  Project_Profile: "",
+  Key_Facts_About_The_Project: "",
+  },
+  Vistors_Information = {
+   Open_To_Public: false,
+   Reception:"",
+   Other_Comments:"",
+  }
+};
