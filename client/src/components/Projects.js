bf2f12bd6b8d2edacb6ea224131c7870e6d9bf72
Kseniia <kseniia.ruby@gmail.com>
2021-07-09 14:43:37 -0600

    Projects.js is up to date and ready for Schema

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -7,27 +7,48 @@ export const Projects = {
       Phone_Number: "",
       Project_Website: "",
    },
-  Project_Information = {  
-     Project_Name: "",
-  Image,
-  Location: {
-    Address: "",
-    City: "",
-    Province: "",
-    Postal_Code: "",
-    Coordinates: [],
-  },
-  Neighbourhood: "",
-  Project_Owners_and_Clients: [],
-  Project_Designers_Collaborations: [],
-  Completion_Date: "",
-  Project_Typology: [],
-  Project_Profile: "",
-  Key_Facts_About_The_Project: "",
-  },
-  Vistors_Information = {
-   Open_To_Public: false,
-   Reception:"",
-   Other_Comments:"",
-  }
-};
+   Project_Information: {
+      Project_Name: "",
+      Location: {
+         Address: "",
+         City: "",
+         Province: "",
+         Postal_Code: "",
+         Coordinates: [],
+      },
+      Neighbourhood: "",
+      Project_Owners_and_Clients: [],
+      Project_Designers_Collaborations: [],
+      Completion_Date: "",
+      Project_Typology: [],
+      Project_Profile: "",
+      Key_Facts_About_The_Project: "",
+   },
+   Vistors_Information: {
+      Open_To_Public: false,
+      Reception: "",
+      Other_Comments: "",
+   },
+   Innovation_Descriptions: {
+      Primary_Type_of_Innovation: {
+         Classification: "",
+         Description: ""
+      },
+      Secondary_Type_of_Innovation: {
+         Classification: "",
+         Description: ""
+      },
+      Tertiary_Type_of_Innovation: {
+         Classification: "",
+         Description: ""
+      },
+      General_Profile: "",
+      Key_Facts: [],
+   },
+   Classification_List: {
+      Classification: [],
+      Gateway_Words: [],
+      Feature_Words: []
+   },
+   Image
+}
