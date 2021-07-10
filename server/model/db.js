1440c35fbd521e7073732fc5969f5d478e1c2786
Femi Bode-George <femibodgeorge@gmail.com>
2021-07-10 11:52:16 -0600

    db connected

--- server/model/db.js
+++ server/model/db.js
@@ -1,16 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connection SUCCESS");
-  } catch (error) {
-    console.error("MongoDB connection FAIL");
-    process.exit(1);
-  }
-};
-module.exports = connectDB;
+const uri = process.env.MONGO_URI;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas..."))
+  .catch((err) => console.error("Could not connect to MongoDB Atlas...", err));
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-// require('dotenv').config();
-const app = require('./app')
+require("dotenv").config();
+const app = require("./app");
+require("./model/db");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server listening on port ' + port)
-})
+  console.log("Server listening on port " + port);
+});
