e16a73333090dd5681614fd99c449a7f52f04991
jasonSalazar112 <jrsuperstar81@yahoo.com>
2021-07-16 13:51:24 -0600

    Navbar css update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "../styles/Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 export default function Navbar() {
-   return (
-      <>
-         <div>
-            <div className="navbar-container">
-               <Link to="/" className="nav-links">
-                  Home
-               </Link>
-               {/* <Link to="/" className="nav-links">
+  return (
+    <>
+      <div>
+        <div className="navbar-container">
+          <Link to="/" className="nav-links">
+            Home
+          </Link>
+          <Link to="/" className="nav-links">
             Menu
           </Link>
           <Link to="/" className="nav-links">
             Search
-          </Link> */}
-               <Link to="/MarkPointOnMap" className="nav-links">
-                  Map
-               </Link>
-            </div>
-         </div>
-      </>
-   )
+          </Link>
+          <Link to="/MarkPointOnMap" className="nav-links">
+            Map
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
