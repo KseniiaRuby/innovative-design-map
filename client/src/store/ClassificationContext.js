65a292f898dc8d3b572d6938a75fa3e905499576
JRSalazar223 <>
2021-07-27 02:27:25 -0600

    Gateway header update

--- client/src/store/ClassificationContext.js
+++ client/src/store/ClassificationContext.js
@@ -3,19 +3,12 @@ import { createContext } from "react";
 const ClassificationContext = createContext({
   // All available gateway words
   allGatewayWords: [],
-  
+
   // Initialize gateway word
-  //
   gatewayWord: undefined,
 
   // Initialize classification
-  //
   classification: undefined,
-
-  setGatewayWord: () => {},
-  setClassification: () => {},
-
-  getAllClassifications: () => {},
 });
 
 export default ClassificationContext;
