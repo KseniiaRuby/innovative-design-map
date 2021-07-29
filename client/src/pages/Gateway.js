b0050d3aa77ca3618d57e623dc6c7de85d0e8837
jasonSalazar112 <jrsuperstar81@yahoo.com>
2021-07-29 14:50:29 -0600

    Conditional formatting gateway button

--- client/src/pages/Gateway.js
+++ client/src/pages/Gateway.js
@@ -1,40 +1,52 @@
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import ClassificationContext from "../store/ClassificationContext"
-import "../styles/Styles.css"
+import { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ClassificationContext from "../store/ClassificationContext";
+import "../styles/Styles.css";
 
 export function Gateway() {
-   const classificationCtx = useContext(ClassificationContext)
-   let selectedGateway = ""
+  const [gateway, setGateway] = useState();
+  const classificationCtx = useContext(ClassificationContext);
+  //   let selectedGateway = "";
 
-   return (
-      <div className="choose-gateway-word-title">
-         What <br /> makes <br />Calgary
-         <div className="list-spacer">
-            <select
-               className="list-style"
-               value={classificationCtx.gatewayWord}
-               onChange={(e) => {
-                  selectedGateway = e.target.value
-                  classificationCtx.setGatewayWord(selectedGateway)
-               }}
-            >
-               <option value=""></option>
-               {classificationCtx.allGatewayWords.map((gatewayWord) => {
-                  return <option key={gatewayWord}>{gatewayWord}</option>
-               })}
-            </select>
-            &nbsp;?
-            <div>
-               <div className="button-spacer">
-                  <button className="button-lets-go">
-                     <Link className="button-styling" to="/Category">
-                        LET'S GO
-                     </Link>
-                  </button>
-               </div>
-            </div>
-         </div>
+  return (
+    <div className="choose-gateway-word-title">
+      What <br /> makes <br />
+      Calgary
+      <div className="list-spacer">
+        <select
+          className="list-style"
+          value={classificationCtx.gatewayWord}
+          onChange={(e) => {
+            // selectedGateway = e.target.value;
+            setGateway(e.target.value);
+            // classificationCtx.setGatewayWord(selectedGateway);
+            classificationCtx.setGatewayWord(e.target.value);
+          }}
+        >
+          <option value=""></option>
+          {classificationCtx.allGatewayWords.map((gatewayWord) => {
+            return <option key={gatewayWord}>{gatewayWord}</option>;
+          })}
+        </select>
+        &nbsp;?
+        <div>
+          <div className="button-spacer">
+            {/*  */}
+            {gateway !== undefined ? (
+              <reactfragment>
+                {/*  */}
+                <button className="button-lets-go">
+                  <Link className="button-styling" to="/Category">
+                    LET'S GO
+                  </Link>
+                </button>
+                {/*  */}
+              </reactfragment>
+            ) : null}
+            {/*  */}
+          </div>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
