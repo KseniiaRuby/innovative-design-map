2a9b67c49cf35795962ec258f5d47f47b993ef96
JRSalazar223 <>
2021-07-29 10:45:37 -0600

    Project Header styling

--- client/src/pages/ProjectPage/components/ProjectHeader.js
+++ client/src/pages/ProjectPage/components/ProjectHeader.js
@@ -7,19 +7,39 @@ const ProjectHeader = ({
   projectImageAlt,
 }) => {
   var sectionStyle = {
-    width: "100%",
-    height: "200%",
+    // width: "100%",
+    // height: "200%",
     background: "url(" + { projectImageBackground } + ")",
     //   backgroundImage:
     //     "https://i.ibb.co/LnHDd7y/Riverdale-Avenue-Retaining-Wall-Replacement-Project-1.jpg",
   };
 
   return (
-    <div className="border-bottom  project-header" style={sectionStyle}>
-      {projectName}
-      {/* <img src={projectImageBackground} alt={projectImageAlt} /> */}
+    // <div className="border-bottom  project-header">
+    <div
+      className="border-bottom  project-header"
+      // style={sectionStyle}
+      // background={"url(" + { projectImageBackground } + ")"}
+      // background-image={"url(" + { projectImageBackground } + ")"}
+    >
+      <div className="project-container">
+        <div className="project-title"> {projectName} </div>
+        {/* <div className="project-image"> */}
+
+        <img
+          src={projectImageBackground}
+          alt={projectImageAlt}
+          style={{ width: "75%" }}
+          max-width={"inherit"}
+          max-height={"inherit"}
+          height={"inherit"}
+          width={"inherit"}
+          object-fit={"cover"}
+          float={"right"}
+        />
+      </div>
+      {/* </div> */}
       {/* New Central Library */}
-      <br />
       <br />
       <br />
       {/* <div className="project-buttons">
