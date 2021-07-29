0f92689f82c9559b08559203fd500ac17b2c0393
jasonSalazar112 <jrsuperstar81@yahoo.com>
2021-07-29 15:05:30 -0600

    Styles button

--- client/src/pages/ProjectPage/components/ProjectAtGlance.js
+++ client/src/pages/ProjectPage/components/ProjectAtGlance.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import '../../../styles/Styles.css'
+import React from "react";
+import "../../../styles/Styles.css";
 
 const ProjectAtGlance = () => {
-   return (
-      <div className="border-bottom project-at-glance">
-         <p><u>At a glance</u></p>
-         <div>
-            Public Space, Leed Gold, Stormwater Management
-         </div>
+  return (
+    <div className="border-bottom project-at-glance">
+      <p>
+        <u>At a glance</u>
+      </p>
+      <div>
+        <p>Public Space, Leed Gold, Stormwater Management</p>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-export default ProjectAtGlance
+export default ProjectAtGlance;
--- client/src/pages/ProjectPage/components/ProjectRelatedFeatures.js
+++ client/src/pages/ProjectPage/components/ProjectRelatedFeatures.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import '../../../styles/Styles.css'
-
-
+import React from "react";
+import "../../../styles/Styles.css";
 
 const ProjectRelatedFeatures = () => {
-   return (
-      <div className="border-bottom border-top project-related-features">
-         <p>
-            <u className="force-left">Features</u>
-            <br />
-            Culture, heritage and Society
-            <br />
-            Transporattion and Infrastructure
-         </p>
-      </div>
-   )
-}
+  return (
+    <div className="border-bottom border-top project-related-features">
+      <p>
+        <u className="force-left">Features</u>
+        <br />
+        <br />
+        Culture, heritage and Society
+        <br />
+        Transporattion and Infrastructure
+      </p>
+    </div>
+  );
+};
 
-export default ProjectRelatedFeatures
+export default ProjectRelatedFeatures;
