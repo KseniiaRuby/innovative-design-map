b872e4f72324c955c0e0aa6be9c1667f70e26303
JRSalazar223 <>
2021-08-03 22:45:04 -0600

    Update Project Description

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,19 +1,19 @@
 // import ScrollMenu from "react-horizontal-scrolling-menu";
-import Header from "../components/Header"
-import MarkPointOnMap from "../components/MarkPointOnMap"
-import Navbar from "../components/Navbar"
-import "../styles/Styles.css"
-import HorizontalProjectMenu from "../components/HorizontalProjectSummaries/HorizontalProjectMenu"
-import MarkPointOnMap2 from "../components/MarkPointOnMap2"
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import "../styles/Styles.css";
+import HorizontalProjectMenu from "../components/HorizontalProjectSummaries/HorizontalProjectMenu";
+import MarkPointOnMap from "../components/MarkPointOnMap";
+import MarkPointOnMap2 from "../components/MarkPointOnMap2";
 
 export default function Map() {
-   return (
-      <div>
-         {/* <MarkPointOnMap2 /> */}
-         <MarkPointOnMap />
-         <Header />
-         <HorizontalProjectMenu />
-         <Navbar />
-      </div>
-   )
+  return (
+    <div>
+      {/* <MarkPointOnMap2 /> */}
+      <MarkPointOnMap />
+      <Header />
+      <HorizontalProjectMenu />
+      <Navbar />
+    </div>
+  );
 }
