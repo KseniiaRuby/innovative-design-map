5bfe464fcbcf8a1a14c93e253fab9abc2d10cb91
JRSalazar223 <>
2021-08-07 12:26:05 -0600

    Glossary fetch activated

--- client/src/store/GlossaryProvider.js
+++ client/src/store/GlossaryProvider.js
@@ -1,53 +1,66 @@
-import GlossaryContext from "./GlossaryContext"
-import { useState, useEffect } from "react"
+import GlossaryContext from "./GlossaryContext";
+import { useState, useEffect } from "react";
 
 export default function GlossaryProvider({ children }) {
-   const [allDefintions, setAllDefinitions] = useState([])
-   const [allTerms, setAllTerms] = useState([])
-   const [selectedDefinition, setSelectedDefinition] = useState()
+  const [allDefintions, setAllDefinitions] = useState([]);
+  const [allTerms, setAllTerms] = useState([]);
+  const [selectedDefinition, setSelectedDefinition] = useState();
 
-   useEffect(() => {
-      const getAllDefinitions = async () => {
-         setAllDefinitions([
-            { term: 'Project', description: 'The word PROJECT is very important to this application, it represents the main unit of organization to the information and ultimately refers to a single location that was intentionally built up to acheive and urban design effect as well as serve an important city function.' },
-            { term: 'Of The', description: 'a common conjunction' },
-            { term: 'Apple', description: 'A delicious fruit associated with good health.' },
-            { term: 'Banana', description: 'A delicious fruit associated with tropical locations.' }
-         ])
-         try {
-            let response = await fetch("/api/glossary")
-            if (response.status !== 200) {
-               throw new Error("Fetch for glossary words failed")
-            }
-            let defintions = await response.json()
-            setAllDefinitions(defintions)
-         } catch (err) {
-            console.log("Error on Client-Side: Glossary Defintions", err)
-         }
+  useEffect(() => {
+    const getAllDefinitions = async () => {
+      setAllDefinitions([
+        {
+          term: "Project",
+          description:
+            "The word PROJECT is very important to this application, it represents the main unit of organization to the information and ultimately refers to a single location that was intentionally built up to acheive and urban design effect as well as serve an important city function.",
+        },
+        { term: "Of The", description: "a common conjunction" },
+        {
+          term: "Apple",
+          description: "A delicious fruit associated with good health.",
+        },
+        {
+          term: "Banana",
+          description: "A delicious fruit associated with tropical locations.",
+        },
+      ]);
+      try {
+        let response = await fetch("/api/glossary");
+        if (response.status !== 200) {
+          throw new Error("Fetch for glossary words failed");
+        }
+        let definitions = await response.json();
+        setAllDefinitions(definitions);
+        console.log("All Glossary: " + JSON.stringify(definitions));
+      } catch (err) {
+        console.log("Error on Client-Side: Glossary Defintions", err);
       }
+    };
 
-      getAllDefinitions()
-   }, [])
+    getAllDefinitions();
+  }, []);
 
-   useEffect(() => {
-      let terms = allDefintions.map((definition) => definition.term)
-      setAllTerms(terms)
-   }, [allDefintions])
+  useEffect(() => {
+    let terms = allDefintions.map((definition) => definition.term);
+    setAllTerms(terms);
+  }, [allDefintions]);
 
-   const setSelectedTerm = (term) => {
-      let foundDefintion = allDefintions.find((definition) => definition.term === term)
-      setSelectedDefinition(foundDefintion)
-   }
+  const setSelectedTerm = (term) => {
+    let foundDefintion = allDefintions.find(
+      (definition) => definition.term === term
+    );
+    setSelectedDefinition(foundDefintion);
+  };
 
-   return (
-      <GlossaryContext.Provider
-         value={{
-            allTerms,
-            setSelectedTerm,
-            selectedDefinition
-         }}
-      >
-         {children}
-      </GlossaryContext.Provider>
-   )
+  return (
+    <GlossaryContext.Provider
+      value={{
+        allTerms,
+        setSelectedTerm,
+        selectedDefinition,
+      }}
+    >
+      {children}
+    </GlossaryContext.Provider>
+  );
 }
--- server/controller/glossaryControllers.js
+++ server/controller/glossaryControllers.js
@@ -1,29 +1,29 @@
-const Glossary = require("../models/Glossary")
+const Glossary = require("../models/Glossary");
 
 const getGlossary = async (req, res) => {
-   try {
-      const Glossary = await Glossary.find({})
-      res.json(glossary)
-   } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: "Server Error" })
-   }
-}
+  try {
+    const glossary = await Glossary.find({});
+    res.json(glossary);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 const newGlossary = async (req, res) => {
-   let newGlossary = req.body
-   try {
-      const glossary = new Glossary(newGlossary)
-      await glossary.save()
-      console.log("Created Glossary", glossary)
-      res.send(glossary)
-   } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: "Server Error" })
-   }
-}
+  let newGlossary = req.body;
+  try {
+    const glossary = new Glossary(newGlossary);
+    await glossary.save();
+    console.log("Created Glossary", glossary);
+    res.send(glossary);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 module.exports = {
-   getGlossary,
-   newGlossary,
-}
+  getGlossary,
+  newGlossary,
+};
