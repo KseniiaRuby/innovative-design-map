1ae706f4d88735c5ed09612aa7325d2ededd4a73
Femi Bode-George <femibodgeorge@gmail.com>
2021-08-07 11:07:17 -0600

    updated schema glossary

--- server/models/Glossary.js
+++ server/models/Glossary.js
@@ -1,16 +1,11 @@
-require("./db")
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-
+require("./db");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //We need Schema for Glossary here
 const glossarySchema = new Schema({
-   glossaryName: String,
-   glossaryDescription: String,
-})
+  term: String,
+  description: String,
+});
 
-module.exports = mongoose.model(
-   "Glossary",
-   glossarySchema,
-   "Glossary"
- );
+module.exports = mongoose.model("Glossary", glossarySchema, "Glossary");
