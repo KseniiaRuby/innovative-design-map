094284ee2bf61fb48a72c8b32c876be3b31643f2
Kseniia <kseniia.ruby@gmail.com>
2021-08-10 12:02:11 -0600

    Project Page: changed the Project name to be adjustable size depending on the length of the name of the project

--- client/src/pages/ProjectPage/components/ProjectHeader.js
+++ client/src/pages/ProjectPage/components/ProjectHeader.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+// import { Link } from "react-router-dom"
 import "../../../styles/Styles.css"
 
 const ProjectHeader = ({
@@ -7,12 +7,29 @@ const ProjectHeader = ({
    projectImageBackground,
    projectImageAlt,
 }) => {
+
+   const [titleStyle, setTitleStyle] = useState({})
+
+   useEffect(() => {
+      const outerHeight = document.getElementById("outer").clientHeight
+      const titleDiv = document.getElementById("title")
+      if (titleDiv.clientHeight > outerHeight) {
+         ResizeToFitText(titleDiv, outerHeight)
+      }
+   }, [projectName])
+
+   const ResizeToFitText = (element, outerHeight) => {
+      const fontSize = window.getComputedStyle(element).fontSize
+      element.style.fontSize = parseInt(fontSize) - 0.1 + "px"
+      if (element.clientHeight > outerHeight) {
+         ResizeToFitText(element, outerHeight)
+      }
+   }
+
    const sectionStyle = {
       // width: "100%",
-      // height: "200%",
+      height: "100%",
       background: "url(" + { projectImageBackground } + ")"
-      //   backgroundImage:
-      //     "https://i.ibb.co/LnHDd7y/Riverdale-Avenue-Retaining-Wall-Replacement-Project-1.jpg",
    }
 
    return (
@@ -23,14 +40,13 @@ const ProjectHeader = ({
          // background={"url(" + { projectImageBackground } + ")"}
          // background-image={"url(" + { projectImageBackground } + ")"}
          >
-            <div className="project-container">
-               <div className="project-title"> {projectName} </div>
-               {/* <div className="project-image"> */}
-
+            <div className="project-container" id="outer">
+               <div className="project-title" id="title"> {projectName} </div>
                <img
+                  id="pict"
                   src={projectImageBackground}
                   alt={projectImageAlt}
-                  style={{ width: "75%", padding: "0em 0em 0em 3em" }}
+                  style={{ width: "75%", padding: "0vw 0vw 0vw 5vw", float: "right" }}
                // max-width={"inherit"}
                // max-height={"inherit"}
                // height={"inherit"}
@@ -38,18 +54,19 @@ const ProjectHeader = ({
                //  object-fit={"cover"}
                //  float={"right"}
                />
+
             </div>
             {/* </div> */}
             {/* New Central Library */}
             <br />
             <br />
          </div>
-         <div className="project-buttons">
-            {/* <button>CHECK IN</button> */}
+         {/* work on LIke and Share once it's on heroku */}
+         {/* <div className="project-buttons">
+            <button>CHECK IN</button>
             <Link to="" className="nav-links">Like</Link>
             <Link to="" className="nav-links">Share</Link>
-
-         </div>
+         </div> */}
       </div>
    )
 }
