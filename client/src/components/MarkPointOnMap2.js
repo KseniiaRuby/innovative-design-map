1a7e29a1bbf37bb9e65be6815ed9fe37e2826216
Femi Bode-George <femibodgeorge@gmail.com>
2021-08-11 09:21:05 -0600

    dark map

--- client/src/components/MarkPointOnMap2.js
+++ client/src/components/MarkPointOnMap2.js
@@ -1,5 +1,5 @@
 import mapboxgl from "mapbox-gl";
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import React, { useRef, useEffect, useContext } from "react";
 import ClassificationContext from "../store/ClassificationContext";
 
@@ -9,83 +9,81 @@ import "../styles/MarkPointOnMap2.css";
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function MarkPointOnMap2({ setSelectedProjectIndex, selectedProjectOnSlide }) {
-   // let [markerColor, setMarkerColor] = useState("white")
-   const mapContainer = useRef(null);
-   const coordinates = useRef(null);
-   const classificationCtx = useContext(ClassificationContext);
-   const features = Array.from(classificationCtx.projects);
+  // let [markerColor, setMarkerColor] = useState("white")
+  const mapContainer = useRef(null);
+  const coordinates = useRef(null);
+  const classificationCtx = useContext(ClassificationContext);
+  const features = Array.from(classificationCtx.projects);
 
-   // useEffect(() => {
-   //    markerColor = "white"
-   //    if (selectedProjectOnSlide) {
+  // useEffect(() => {
+  //    markerColor = "white"
+  //    if (selectedProjectOnSlide) {
 
-   //          setMarkerColor("red")
-   //    }
-   // }, [selectedProjectOnSlide])
+  //          setMarkerColor("red")
+  //    }
+  // }, [selectedProjectOnSlide])
 
-   useEffect(() => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/femibodegeorgeg/cks6lqlra6vmv17p669fj7r46",
+      center: [-114.066666, 51.049999],
+      zoom: 10,
+      attributionControl: true,
+      // }
+      //    .addControl(map.geocoderControl('mapbox.places'))
+    });
 
-      const map = new mapboxgl.Map({
-         container: mapContainer.current,
-         style: "mapbox://styles/mapbox/dark-v10",
-         center: [-114.066666, 51.049999],
-         zoom: 10,
-         attributionControl: true
-         // }
-         //    .addControl(map.geocoderControl('mapbox.places'))
-      });
-
-      map.addControl(
-         new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-         }));
-
-      // Add zoom and rotation controls to the map.
-      map.addControl(new mapboxgl.NavigationControl(
-         // {
-         //    visualizePitch: true,
-         //    showZoom: true,
-         //    showCompass: true
-         // }
-      ));
-
-
-      // Add geolocate control to the map.
-      map.addControl(
-         new mapboxgl.GeolocateControl({
-            positionOptions: {
-               enableHighAccuracy: true,
-            },
-            trackUserLocation: true,
-            showUserLocation: true,
-            showAccuracyCircle: true,
-            showUserHeading: true
-
-         })
-      );
-      // features.forEach((feature, index) => {
-      features.forEach((feature, index) => {
-         var el = document.createElement('div');
-         el.className = `markers`;
-         new mapboxgl.Marker(el)
-            .setLngLat(feature.location.coordinates)
-            .addTo(map)
-         el.addEventListener('click', () => {
-            // marker.getElement().addEventListener('click', () => {
-            // alert("Clicked on" + index);
-            setSelectedProjectIndex(index)
-         });
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
       })
+    );
 
-   }, [classificationCtx.projects])
+    // Add zoom and rotation controls to the map.
+    map.addControl(
+      new mapboxgl.NavigationControl()
+      // {
+      //    visualizePitch: true,
+      //    showZoom: true,
+      //    showCompass: true
+      // }
+    );
 
-   return (
-      <div className="map-spacer-top">
-         <div className="map-container" ref={mapContainer} />
-         <div className="map-spacer-bottom"></div>
-      </div>
-   );
+    // Add geolocate control to the map.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserLocation: true,
+        showAccuracyCircle: true,
+        showUserHeading: true,
+      })
+    );
+    // features.forEach((feature, index) => {
+    features.forEach((feature, index) => {
+      var el = document.createElement("div");
+      el.className = `markers`;
+      new mapboxgl.Marker(el)
+        .setLngLat(feature.location.coordinates)
+        .addTo(map);
+      el.addEventListener("click", () => {
+        // marker.getElement().addEventListener('click', () => {
+        // alert("Clicked on" + index);
+        setSelectedProjectIndex(index);
+      });
+    });
+  }, [classificationCtx.projects]);
+
+  return (
+    <div className="map-spacer-top">
+      <div className="map-container" ref={mapContainer} />
+      <div className="map-spacer-bottom"></div>
+    </div>
+  );
 }
 
 export default MarkPointOnMap2;
--- client/src/components/MarkPointOnMap2copy.js
+++ client/src/components/MarkPointOnMap2copy.js
@@ -1,124 +1,118 @@
-import mapboxgl from "mapbox-gl"
-import React, { useRef, useState, useEffect, useContext } from "react"
-import { Link } from "react-router-dom"
-import ClassificationContext from "../store/ClassificationContext"
+import mapboxgl from "mapbox-gl";
+import React, { useRef, useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import ClassificationContext from "../store/ClassificationContext";
 // import "../styles/Styles.css"
 // import ReactMapGL, { Marker, Popup } from "react-map-gl"
-import projectData from '../data/projectsampleinfo.json'
-import '../styles/MarkPointOnMap2.css'
+import projectData from "../data/projectsampleinfo.json";
+import "../styles/MarkPointOnMap2.css";
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function MarkPointOnMap2() {
-   const classificationCtx = useContext(ClassificationContext)
-  
-   const mapContainer = useRef(null);
-   // const [markerColor, setMarkerColor] = useState('green');
-   const [selectProject, setSelectProject] = useState(null)
+  const classificationCtx = useContext(ClassificationContext);
 
-   // const features = Array.from(classificationCtx.projects)
-   const features = Array.from(projectData.features)
+  const mapContainer = useRef(null);
+  // const [markerColor, setMarkerColor] = useState('green');
+  const [selectProject, setSelectProject] = useState(null);
 
-   document.createElement("div").addEventListener('click', () => 
-   { 
-      alert("Marker Clicked.");
-   }
-   ); 
+  // const features = Array.from(classificationCtx.projects)
+  const features = Array.from(projectData.features);
 
+  document.createElement("div").addEventListener("click", () => {
+    alert("Marker Clicked.");
+  });
 
-   useEffect(() => {
-      // if (map.current) return; // initialize map only once
-      const userGatewayWordChoice = classificationCtx.gatewayWord;
-      const currentClassification = classificationCtx.classification
+  useEffect(() => {
+    // if (map.current) return; // initialize map only once
+    const userGatewayWordChoice = classificationCtx.gatewayWord;
+    const currentClassification = classificationCtx.classification;
 
-      let renderMarkerColor = ''
+    let renderMarkerColor = "";
 
-      const map = new mapboxgl.Map({
-         container: mapContainer.current,
-         style: 'mapbox://styles/mapbox/dark-v10',
-         center: [-114.066666, 51.049999],
-         zoom: 14,
-         bearing: -12
-      });
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/dark-v10",
+      center: [-114.066666, 51.049999],
+      zoom: 14,
+      bearing: -12,
+    });
 
-      // Add zoom and rotation controls to the map.
-      map.addControl(new mapboxgl.NavigationControl());
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
 
-      // Add geolocate control to the map.
-      map.addControl(
-         new mapboxgl.GeolocateControl({
-            positionOptions: {
-               enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserLocation: true,
-            showAccuracyCircle: true
-            
-         })
-      );
+    // Add geolocate control to the map.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserLocation: true,
+        showAccuracyCircle: true,
+      })
+    );
 
-      // switch (currentClassification.classificationName) {
-      //    case "Climate and Environment":
-      //       renderMarkerColor = "green"
-      //       console.log("Current gatewayWord", userGatewayWordChoice)
-      //       console.log("Current Classification", currentClassification.classificationName)
-      //       break;
-      //    case "Energy and Resources":
-      //       renderMarkerColor = "red"
-      //       // setMarkerColor(markerColor)
-      //       console.log("Energy And Resources", currentClassification);
-      //       break;
-      //    case "Culture, Heritage and Society":
-      //       renderMarkerColor = 'yellow'
-      //       console.log("Culture, Heritage and Society");
-      //       break;
-      //    case "Equity and Social Justice":
-      //       renderMarkerColor = "blue"
-      //       console.log("Equity And Social justice");
-      //       break;
-      //    case "Health and Wellness":
-      //       renderMarkerColor = "indigo"
-      //       console.log("Health and Wellness");
-      //       break;
-      //    case "Technology":
-      //       renderMarkerColor = "Orange"
-      //       console.log("Technology");
-      //       break;
-      //    case "Transportation and Infrastructure":
-      //       renderMarkerColor = "violet"
-      //       console.log("Transportation and Infrastructure");
-      //       break;
-      //    default:
-      //       console.log('This Selection Is Incorrect');
-      // }
-      const popup = new mapboxgl.Popup({offset: 25})
-      .setText("Construction on the Washington Monument began in 1848.");
+    // switch (currentClassification.classificationName) {
+    //    case "Climate and Environment":
+    //       renderMarkerColor = "green"
+    //       console.log("Current gatewayWord", userGatewayWordChoice)
+    //       console.log("Current Classification", currentClassification.classificationName)
+    //       break;
+    //    case "Energy and Resources":
+    //       renderMarkerColor = "red"
+    //       // setMarkerColor(markerColor)
+    //       console.log("Energy And Resources", currentClassification);
+    //       break;
+    //    case "Culture, Heritage and Society":
+    //       renderMarkerColor = 'yellow'
+    //       console.log("Culture, Heritage and Society");
+    //       break;
+    //    case "Equity and Social Justice":
+    //       renderMarkerColor = "blue"
+    //       console.log("Equity And Social justice");
+    //       break;
+    //    case "Health and Wellness":
+    //       renderMarkerColor = "indigo"
+    //       console.log("Health and Wellness");
+    //       break;
+    //    case "Technology":
+    //       renderMarkerColor = "Orange"
+    //       console.log("Technology");
+    //       break;
+    //    case "Transportation and Infrastructure":
+    //       renderMarkerColor = "violet"
+    //       console.log("Transportation and Infrastructure");
+    //       break;
+    //    default:
+    //       console.log('This Selection Is Incorrect');
+    // }
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "Construction on the Washington Monument began in 1848."
+    );
 
-      features.forEach(
-         (features) => new mapboxgl.Marker({
-            color: "white",
-            scale: ".5",
-            interactive: true,
-         })
-            .setLngLat(features.geometry.coordinates)
-            .addTo(map)
-            .setPopup(popup)
-      )
-      // mapboxgl.Marker.getElement().addEventListener('click', function (e) { console.log("marker clicked"); });
-      map.on('click', function(e) {
+    features.forEach((features) =>
+      new mapboxgl.Marker({
+        color: "white",
+        scale: ".5",
+        interactive: true,
+      })
+        .setLngLat(features.geometry.coordinates)
+        .addTo(map)
+        .setPopup(popup)
+    );
+    // mapboxgl.Marker.getElement().addEventListener('click', function (e) { console.log("marker clicked"); });
+    map.on("click", function (e) {
+      console.log("Clicking the marker now", e);
 
-         console.log("Clicking the marker now", e);
-     
-         //   map.featuresAt(e.point, {radius: 100, layer: 'YOUR MARKER LAYER ID'}, function(err, features) {
-         //         console.log(features[0]);
-     
-         //   });
-     
-         });
+      //   map.featuresAt(e.point, {radius: 100, layer: 'YOUR MARKER LAYER ID'}, function(err, features) {
+      //         console.log(features[0]);
 
-   });
+      //   });
+    });
+  });
 
-   return <div className="map-container" ref={mapContainer} />;
+  return <div className="map-container" ref={mapContainer} />;
 }
 
-export default MarkPointOnMap2
+export default MarkPointOnMap2;
