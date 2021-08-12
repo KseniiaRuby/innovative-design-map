108db392e78536acb6c2f253a0c17e09efb5b6f7
Kseniia <kseniia.ruby@gmail.com>
2021-08-12 13:50:08 -0600

    set full size image to slider, fixed margins on map page

--- client/src/components/HorizontalProjectSummaries/Button.js
+++ client/src/components/HorizontalProjectSummaries/Button.js
@@ -2,10 +2,15 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "../../styles/Styles.css"
 
-export const Button = ({ children, type, onClick, linkProperty }) => {
+export const Button = ({ children, type, onClick, linkProperty, imageUrl }) => {
    const link = linkProperty ? linkProperty : "/ProjectPage"
    return (
-      <button className={"btn-large"} onClick={onClick} type={type}>
+      <button
+         style={{
+            'background-image':
+               `url(${imageUrl})`
+         }}
+         className={"btn-large"} onClick={onClick} type={type}>
          <Link to={link} className="btn-text">
             {children}
          </Link>
--- client/src/components/MarkPointOnMap2.js
+++ client/src/components/MarkPointOnMap2.js
@@ -1,85 +1,85 @@
-import mapboxgl from "mapbox-gl";
-import React, { useRef, useEffect, useState, useContext } from "react";
-import ClassificationContext from "../store/ClassificationContext";
-import "../styles/Styles.css";
-import "../styles/MarkPointOnMap2.css";
+import mapboxgl from "mapbox-gl"
+import React, { useRef, useEffect, useState, useContext } from "react"
+import ClassificationContext from "../store/ClassificationContext"
+import "../styles/Styles.css"
+import "../styles/MarkPointOnMap2.css"
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 function MarkPointOnMap2({ setSelectedProjectIndex, selectedProjectIndex }) {
-  const mapContainer = useRef(null);
-  const classificationCtx = useContext(ClassificationContext);
-  const features = Array.from(classificationCtx.projects);
-  const [map, setMap] = useState(null);
+   const mapContainer = useRef(null)
+   const classificationCtx = useContext(ClassificationContext)
+   const features = Array.from(classificationCtx.projects)
+   const [map, setMap] = useState(null)
 
-  useEffect(() => {
-    let pins = [];
-    if (map) {
-      features.forEach((feature, index) => {
-        let el = document.createElement("div");
-        pins.push(el);
-        if (index === selectedProjectIndex) {
-          el.className = `red-marker`;
-        } else {
-          el.className = `markers`;
-        }
-        new mapboxgl.Marker(el)
-          .setLngLat(feature.location.coordinates)
-          .addTo(map);
-        el.addEventListener("click", () => {
-          // marker.getElement().addEventListener('click', () => {
-          // alert("Clicked on" + index);
-          setSelectedProjectIndex(index);
-        });
-      });
-    }
-    const cleanup = () => {
-      pins.forEach((pin) => pin.remove());
-      console.log("cleaning up");
-    };
-    return cleanup;
-  }, [selectedProjectIndex, map]);
+   useEffect(() => {
+      let pins = []
+      if (map) {
+         features.forEach((feature, index) => {
+            let el = document.createElement("div")
+            pins.push(el)
+            if (index === selectedProjectIndex) {
+               el.className = `red-marker`
+            } else {
+               el.className = `markers`
+            }
+            new mapboxgl.Marker(el)
+               .setLngLat(feature.location.coordinates)
+               .addTo(map)
+            el.addEventListener("click", () => {
+               // marker.getElement().addEventListener('click', () => {
+               // alert("Clicked on" + index);
+               setSelectedProjectIndex(index)
+            })
+         })
+      }
+      const cleanup = () => {
+         pins.forEach((pin) => pin.remove())
+         console.log("cleaning up")
+      }
+      return cleanup
+   }, [selectedProjectIndex, map])
 
-  useEffect(() => {
-    const newMap = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/femibodegeorgeg/cks6lqlra6vmv17p669fj7r46",
-      center: [-114.066666, 51.049999],
-      zoom: 10,
-      attributionControl: true,
-    });
-
-    // Add zoom and rotation controls to the map.
-    newMap.addControl(
-      new mapboxgl.NavigationControl({
-        //    visualizePitch: true,
-        showZoom: true,
-        showCompass: true,
+   useEffect(() => {
+      const newMap = new mapboxgl.Map({
+         container: mapContainer.current,
+         style: "mapbox://styles/femibodegeorgeg/cks6lqlra6vmv17p669fj7r46",
+         center: [-114.066666, 51.049999],
+         zoom: 10,
+         attributionControl: true,
       })
-    );
 
-    // Add geolocate control to the map.
-    newMap.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-        showUserLocation: true,
-        showAccuracyCircle: true,
-        showUserHeading: true,
-      })
-    );
+      // Add zoom and rotation controls to the map.
+      newMap.addControl(
+         new mapboxgl.NavigationControl({
+            //    visualizePitch: true,
+            showZoom: true,
+            showCompass: true,
+         })
+      )
 
-    setMap(newMap);
-  }, [classificationCtx.projects]);
+      // Add geolocate control to the map.
+      newMap.addControl(
+         new mapboxgl.GeolocateControl({
+            positionOptions: {
+               enableHighAccuracy: true,
+            },
+            trackUserLocation: true,
+            showUserLocation: true,
+            showAccuracyCircle: true,
+            showUserHeading: true,
+         })
+      )
 
-  return (
-    <div className="map-spacer-top">
-      <div className="map-container" ref={mapContainer} />
-      <div className="map-spacer-bottom"></div>
-    </div>
-  );
+      setMap(newMap)
+   }, [classificationCtx.projects])
+
+   return (
+      <div className="map-spacer-top">
+         <div className="map-container" ref={mapContainer} />
+         <div className="map-spacer-bottom"></div>
+      </div>
+   )
 }
 
-export default MarkPointOnMap2;
+export default MarkPointOnMap2
