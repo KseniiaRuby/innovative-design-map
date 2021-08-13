bf681ba2ae559d12d86a29f745fbccd48b1691cb
jasonSalazar112 <jrsuperstar81@yahoo.com>
2021-08-13 17:56:36 -0600

    Removed underline Calgary

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,33 @@
-import { useContext } from "react"
-import ClassificationContext from "../store/ClassificationContext"
-import "../styles/Styles.css"
+import { useContext } from "react";
+import ClassificationContext from "../store/ClassificationContext";
+import "../styles/Styles.css";
 
 export default function Header() {
-   const classificationCtx = useContext(ClassificationContext)
-   let selectedGateway = ""
+  const classificationCtx = useContext(ClassificationContext);
+  let selectedGateway = "";
 
-   return (
-      <>
-         <div>
-            <div className="header-container">
-               <div className="title-text">
-                  What makes <u>Calgary</u>
-                  <select
-                     className="list-style"
-                     value={classificationCtx.gatewayWord}
-                     onChange={(e) => {
-                        selectedGateway = e.target.value
-                        classificationCtx.setGatewayWord(selectedGateway)
-                     }}
-                  >
-                     {classificationCtx.allGatewayWords.map((gatewayWord) => {
-                        return <option key={gatewayWord}>{gatewayWord}</option>
-                     })}
-                  </select>
-                  &nbsp;?
-               </div>
-            </div>
-         </div>
-      </>
-   )
+  return (
+    <>
+      <div>
+        <div className="header-container">
+          <div className="title-text">
+            What makes Calgary{" "}
+            <select
+              className="list-style"
+              value={classificationCtx.gatewayWord}
+              onChange={(e) => {
+                selectedGateway = e.target.value;
+                classificationCtx.setGatewayWord(selectedGateway);
+              }}
+            >
+              {classificationCtx.allGatewayWords.map((gatewayWord) => {
+                return <option key={gatewayWord}>{gatewayWord}</option>;
+              })}
+            </select>
+            &nbsp;?
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
