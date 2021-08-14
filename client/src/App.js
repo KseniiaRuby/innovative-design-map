fa941d3e8c3799ac861030cd82f847b6396829b8
Navin Jetha <navin4life@gmail.com>
2021-08-14 10:51:22 -0600

    cleaned up data folder and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Gateway } from "./pages/Gateway"
 import Category from "./pages/Category"
 import Map from "./pages/Map"
-// import MarkPointOnMap from "./components/MarkPointOnMap"
 import ProjectPage from "./pages/ProjectPage/ProjectPage"
 import ClassificationProvider from "./store/ClassificationProvider"
 import ProjectProvider from "./store/ProjectProvider"
@@ -20,7 +19,6 @@ function App() {
                      <Route path="/Map" component={Map} />
                      <Route path="/ProjectPage/:id" component={ProjectPage} />
                   </Switch>
-                  {/* <Navbar /> */}
                </Router>
             </GlossaryProvider>
          </ClassificationProvider>
