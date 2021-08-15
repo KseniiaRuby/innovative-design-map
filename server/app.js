92e08759dfb254a8507542751ba9aa1b43eec48b
JRSalazar223 <>
2021-08-15 05:42:24 -0600

    Deploy to Heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,4 @@ var app = express();
 
 app.use(express.json());
 
-// Serve the react application
-app.use(express.static("../client/build"));
-
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
-require("dotenv").config()
-const express = require("express")
-const connectDB = require("./models/db")
-const classificationRoutes = require("./routes/classificationRoutes")
-const projectRoutes = require("./routes/projectRoutes")
-const glossaryRoutes = require("./routes/glossaryRoutes")
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./models/db");
+const classificationRoutes = require("./routes/classificationRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const glossaryRoutes = require("./routes/glossaryRoutes");
 
+connectDB();
+const app = express();
+app.use(express.json());
 
-connectDB()
-const app = express()
-app.use(express.json())
+// Serve the react application
+app.use(express.static("../client/build"));
 
 //routes
 
-app.use("/api/classification", classificationRoutes)
-app.use("/api/project", projectRoutes)
-app.use("/api/glossary", glossaryRoutes)
+app.use("/api/classification", classificationRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/glossary", glossaryRoutes);
 
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-   console.log("Server listening on port " + port)
-})
+  console.log("Server listening on port " + port);
+});
