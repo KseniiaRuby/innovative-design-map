5ae507a9488901ff049f688cbc2382f87ee8e519
Kseniia <kseniia.ruby@gmail.com>
2021-08-16 12:32:34 -0600

    changed css on project name & count on slider

--- client/src/components/HorizontalProjectSummaries/ProjectSlider.js
+++ client/src/components/HorizontalProjectSummaries/ProjectSlider.js
@@ -1,64 +1,67 @@
-import { Button } from "./Button";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import "../../styles/Styles.css";
+import { Button } from "./Button"
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io"
+import "../../styles/Styles.css"
 
 const ProjectSlider = ({
-  slides,
-  selectedProjectIndex,
-  setSelectedProjectIndex,
+   slides,
+   selectedProjectIndex,
+   setSelectedProjectIndex,
 }) => {
-  const features = Array.from(slides);
-  const length = features.length;
+   const features = Array.from(slides)
+   const length = features.length
 
-  const nextSlide = () => {
-    setSelectedProjectIndex(
-      selectedProjectIndex === length - 1 ? 0 : selectedProjectIndex + 1
-    );
-  };
+   const nextSlide = () => {
+      setSelectedProjectIndex(
+         selectedProjectIndex === length - 1 ? 0 : selectedProjectIndex + 1
+      )
+   }
 
-  const prevSlide = () => {
-    setSelectedProjectIndex(
-      selectedProjectIndex === 0 ? length - 1 : selectedProjectIndex - 1
-    );
-  };
+   const prevSlide = () => {
+      setSelectedProjectIndex(
+         selectedProjectIndex === 0 ? length - 1 : selectedProjectIndex - 1
+      )
+   }
 
-  if (!Array.isArray(features).length <= 0) {
-    return null;
-  }
+   if (!Array.isArray(features).length <= 0) {
+      return null
+   }
 
-  return (
-    <section className="slider">
-      {features.map((project, index) => {
-        return (
-          <div
-            className={
-              index === selectedProjectIndex ? "slide active" : "slide"
-            }
-            key={index}
-          >
-            <IoIosArrowBack className="left-arrow" onClick={prevSlide} />
-            <IoIosArrowForward className="right-arrow" onClick={nextSlide} />
-            {index === selectedProjectIndex && (
-              <Button
-                imageUrl={project.imageUrls.image1.image}
-                linkProperty={`/ProjectPage/` + project._id}
-              >
-                <div>
-                  <div className="project-name-align">
-                    {project.projectName}{" "}
-                  </div>
-                  <p></p>
-                  <div className="count">
-                    {index + 1} of {length}{" "}
-                  </div>
-                </div>
-              </Button>
-            )}
-          </div>
-        );
-      })}
-    </section>
-  );
-};
+   return (
+      <section className="slider">
+         {features.map((project, index) => {
+            return (
+               <div
+                  className={
+                     index === selectedProjectIndex ? "slide active" : "slide"
+                  }
+                  key={index}
+               >
+                  <IoIosArrowBack className="left-arrow" onClick={prevSlide} />
+                  <IoIosArrowForward className="right-arrow" onClick={nextSlide} />
+                  {index === selectedProjectIndex && (
+                     <Button
+                        imageUrl={project.imageUrls.image1.image}
+                        linkProperty={`/ProjectPage/` + project._id}
+                     >
+                        <div>
+                           <div className="project-name-align">
+                              <div>
+                                 <span className="project-name-slider">
+                                    {project.projectName}{" "}
+                                    <div className="count">
+                                       {index + 1} of {length}{" "}
+                                    </div>
+                                 </span>
+                              </div>
+                           </div>
+                        </div>
+                     </Button>
+                  )}
+               </div>
+            )
+         })}
+      </section>
+   )
+}
 
-export default ProjectSlider;
+export default ProjectSlider
