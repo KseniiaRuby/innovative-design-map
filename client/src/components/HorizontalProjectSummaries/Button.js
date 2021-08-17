4bdd9cd570afcd1e64d0ff7f55f6bbdccebc6c4f
Kseniia <kseniia.ruby@gmail.com>
2021-08-17 11:02:20 -0600

    whole slider is clickable, z-coordinate for arrows on slider is off

--- client/src/components/HorizontalProjectSummaries/Button.js
+++ client/src/components/HorizontalProjectSummaries/Button.js
@@ -1,30 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../../styles/Styles.css";
+import React from "react"
+import { Link } from "react-router-dom"
+import "../../styles/Styles.css"
 
 export const Button = ({ children, type, onClick, linkProperty, imageUrl }) => {
-  const link = linkProperty ? linkProperty : "/ProjectPage";
+   const link = linkProperty ? linkProperty : "/ProjectPage"
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+   const scrollToTop = () => {
+      window.scrollTo({
+         top: 0,
+         behavior: "smooth",
+      })
+   }
 
-  return (
-    <button
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className={"btn-large"}
-      // onClick={onClick}
-      onClick={scrollToTop}
-      type={type}
-    >
-      <Link to={link} className="btn-text">
-        {children}
-      </Link>
-    </button>
-  );
-};
+   return (
+      <div>
+         <Link to={link} className="btn-text">
+            <button
+               style={{
+                  backgroundImage: `url(${imageUrl})`,
+               }}
+               className={"btn-large"}
+               // onClick={onClick}
+               onClick={scrollToTop}
+               type={type}
+            >
+               <div className="project-name-on-slider">
+                  {children}
+               </div>
+            </button>
+         </Link>
+      </div>
+   )
+}
--- client/src/components/HorizontalProjectSummaries/ProjectSlider.js
+++ client/src/components/HorizontalProjectSummaries/ProjectSlider.js
@@ -1,73 +1,71 @@
-import { Button } from "./Button";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import "../../styles/Styles.css";
+import { Button } from "./Button"
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io"
+import "../../styles/Styles.css"
 
 const ProjectSlider = ({
-  slides,
-  selectedProjectIndex,
-  setSelectedProjectIndex,
+   slides,
+   selectedProjectIndex,
+   setSelectedProjectIndex,
 }) => {
-  const features = Array.from(slides);
-  const length = features.length;
+   const features = Array.from(slides)
+   const length = features.length
 
-  const nextSlide = () => {
-    setSelectedProjectIndex(
-      selectedProjectIndex === length - 1 ? 0 : selectedProjectIndex + 1
-    );
-  };
+   const nextSlide = () => {
+      setSelectedProjectIndex(
+         selectedProjectIndex === length - 1 ? 0 : selectedProjectIndex + 1
+      )
+   }
 
-  const prevSlide = () => {
-    setSelectedProjectIndex(
-      selectedProjectIndex === 0 ? length - 1 : selectedProjectIndex - 1
-    );
-  };
+   const prevSlide = () => {
+      setSelectedProjectIndex(
+         selectedProjectIndex === 0 ? length - 1 : selectedProjectIndex - 1
+      )
+   }
 
-  if (!Array.isArray(features).length <= 0) {
-    return null;
-  }
+   if (!Array.isArray(features).length <= 0) {
+      return null
+   }
 
-  return (
-    <div className="slider-container">
-      <div className="slider-container-left">
-        <IoIosArrowBack className="arrow" onClick={prevSlide} />
-      </div>
-      <section className="slider">
-        {features.map((project, index) => {
-          return (
-            <div
-              className={
-                index === selectedProjectIndex ? "slide active" : "slide"
-              }
-              key={index}
-            >
-              {index === selectedProjectIndex && (
-                <Button
-                  imageUrl={project.imageUrls.image1.image}
-                  linkProperty={`/ProjectPage/` + project._id}
-                >
-                  <div>
-                    <div className="project-name-align">
-                      <div>
-                        <span>
-                          {project.projectName}{" "}
-                          <div className="count">
-                            {index + 1} of {length}{" "}
-                          </div>
-                        </span>
-                      </div>
-                    </div>
+   return (
+      <div className="slider-container">
+         <div className="slider-container-left">
+            <IoIosArrowBack className="arrow" onClick={prevSlide} />
+         </div>
+         <section className="slider">
+            {features.map((project, index) => {
+               return (
+                  <div
+                     className={
+                        index === selectedProjectIndex ? "slide active" : "slide"
+                     }
+                     key={index}
+                  >
+                     {index === selectedProjectIndex && (
+                        <Button
+                           imageUrl={project.imageUrls.image1.image}
+                           linkProperty={`/ProjectPage/` + project._id}
+                        >
+                           <div>
+                              <div className="project-name-align">
+                                 <span>
+                                    {project.projectName}{" "}
+                                    <div className="count">
+                                       {index + 1} of {length}{" "}
+                                    </div>
+                                 </span>
+                              </div>
+                           </div>
+                        </Button>
+                     )}
                   </div>
-                </Button>
-              )}
-            </div>
-          );
-        })}
-      </section>
-      <div className="slider-container-right">
-        <IoIosArrowForward className="arrow" onClick={nextSlide} />
+               )
+            })}
+         </section>
+         <div className="slider-container-right">
+            <IoIosArrowForward className="arrow" onClick={nextSlide} />
+         </div>
       </div>
-    </div>
-  );
-};
+   )
+}
 
-export default ProjectSlider;
+export default ProjectSlider
