cf6aac90ef4852e26c99daa784fdb665d468f52b
jasonSalazar112 <jrsuperstar81@yahoo.com>
2021-08-24 09:32:55 -0600

    Added refresh error handling.

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 // Serve the react application
 app.use(express.static("../client/build"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 //routes
 
 app.use("/api/classification", classificationRoutes);
